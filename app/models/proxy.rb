3800a3898df7acedaf51d655812ce0fdd9eafe21
Gloria Budiman <wahyu.g@gmail.com>
2019-03-08 21:21:18 -0800

    Use delegate

--- app/models/proxy.rb
+++ app/models/proxy.rb
@@ -1,14 +1,8 @@
 class Proxy
+  delegate_missing_to :@accessor
+
   def initialize(ancestor, accessor)
     @ancestor = ancestor
     @accessor = ancestor[accessor]
-  end
-
-  def fetch_aggregate
-    @accessor.fetch_aggregate
-  end
-
-  def assign_root(value)
-    @accessor.assign_root(value)
   end
 end
