70637e7b47b12301b6399ad6fc77b93308a470ac
Gloria Budiman <wahyu.g@gmail.com>
2019-03-11 00:15:12 -0700

    Travis and Codecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start
+
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
