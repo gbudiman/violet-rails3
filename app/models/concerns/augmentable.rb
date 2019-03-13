e83718eaee5381d1dfc508c9efda42de1814046a
Gloria Budiman <wahyu.g@gmail.com>
2019-03-13 01:10:03 -0700

    Cleaner delegation

--- app/models/concerns/augmentable.rb
+++ app/models/concerns/augmentable.rb
@@ -4,6 +4,12 @@ module Concerns
   module Augmentable
     extend ActiveSupport::Concern
 
+    class_methods do
+      def extended(base)
+        base.assign_root(identity_value)
+      end
+    end
+
     def method_missing(method_name, *args)
       case method_name[-1]
       when '='
@@ -12,6 +18,24 @@ module Concerns
       else
         self[method_name] || 0
       end
+    end
+
+    def root
+      self[root_accessor]
+    end
+
+    def assign_root(value)
+      self[root_accessor] = value
+    end
+
+    def disable(*args)
+      args.map { |x| self[x] = 0 if self[x].present? }
+      aggregate
+    end
+
+    def enable(*args)
+      args.map { |x| self[x] = cached_value(x) if self[x].present? }
+      aggregate
     end
 
     def active_values
--- app/models/concerns/stat_queryable.rb
+++ app/models/concerns/stat_queryable.rb
@@ -12,30 +12,8 @@ module Concerns
       1
     end
 
-    def self.extended(base)
-      base.assign_root(identity_value)
-    end
-
-    def root
-      self[root_accessor]
-    end
-
-    def assign_root(value)
-      self[root_accessor] = value
-    end
-
     def aggregate
       active_values.reduce(0) { |a, b| a + b }
-    end
-
-    def disable(*args)
-      args.map { |x| self[x] = 0 if self[x].present? }
-      aggregate
-    end
-
-    def enable(*args)
-      args.map { |x| self[x] = cached_value(x) if self[x].present? }
-      aggregate
     end
   end
 end
